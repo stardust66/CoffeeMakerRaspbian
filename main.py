81ee559f284866867fa9e406c3b27b9b1ab98892
Jason Chen <kbtpodifo@gmail.com>
2017-05-19 10:28:46 -0400

    Change mode to BCM
    This makes it easier to work with the cobbler+

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ logging.basicConfig(format='%(asctime)s:%(name)s: %(message)s',
 logger = logging.getLogger("script")
 logger.setLevel(logging.DEBUG)
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # Using GPIO03
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 
 # Launch Indicator
-GPIO.output(7, GPIO.HIGH)
+GPIO.output(23, GPIO.HIGH)
 
 # Initialize TwitterChecker
 t_checker = TwitterChecker(logger)
 w_checker = WebChecker(logger)
 
 def make_coffee():
-    GPIO.output(5, GPIO.HIGH)
+    GPIO.output(18, GPIO.HIGH)
 
     # Pause for five minutes (or how long it take to make coffee)
     time.sleep(300)
-    GPIO.output(5, GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
 
 while True:
     try:
