5576714c7432ff1dd65242709db9a6505f0010db
Jason Chen <kbtpodifo@gmail.com>
2017-05-25 09:10:14 -0400

    Wrap main.py in a class

--- main.py
+++ main.py
@@ -12,45 +12,88 @@ from RPi import GPIO
 # Set timezone
 os.environ["TZ"] = "US/Eastern"
 
-# Configure Log
-LOGDIR = "/home/pi/Documents/coffeemakerraspbian/"
-logger = logging.getLogger("main")
-fh = logging.FileHandler(LOGDIR + "coffee.log", mode="w")
-ih = logging.FileHandler(LOGDIR + "important.log")
-fm = logging.Formatter('%(asctime)s:%(name)s: %(message)s')
+class CoffeeMachine():
+    def __init__(self):
+        # Configure Log
+        LOGDIR = "/home/pi/Documents/coffeemakerraspbian/"
+        fh = logging.FileHandler(LOGDIR + "coffee.log", mode="w")
+        ih = logging.FileHandler(LOGDIR + "important.log")
+        fm = logging.Formatter('%(asctime)s:%(name)s: %(message)s')
 
-fh.setLevel(logging.DEBUG)
-ih.setLevel(logging.WARNING)
-fh.setFormatter(fm)
-ih.setFormatter(fm)
+        fh.setLevel(logging.DEBUG)
+        ih.setLevel(logging.WARNING)
+        fh.setFormatter(fm)
+        ih.setFormatter(fm)
 
-logger = logging.getLogger("main")
-logger.addHandler(fh)
-logger.addHandler(ih)
-logger.setLevel(logging.DEBUG)
+        self.logger = logging.getLogger("main")
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ih)
+        self.logger.setLevel(logging.DEBUG)
 
-logger.debug("Initialized")
+        self.logger.debug("Initialized")
 
-GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
 
-# GPIO18 - Coffee Machine
-# GPIO23 - Script Running Status
-# GPIO16 - Load Indicator
-# GPIO12 - Switch
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # GPIO18 - Coffee Machine
+        # GPIO23 - Script Running Status
+        # GPIO16 - Load Indicator
+        # GPIO12 - Switch
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(23, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Launch Indicator
-GPIO.output(23, GPIO.HIGH)
+        # Launch Indicator
+        GPIO.output(23, GPIO.HIGH)
 
-# Initialize TwitterChecker
-t_checker = TwitterChecker(logger)
-w_checker = WebChecker(logger)
+        # Initialize TwitterChecker
+        self.t_checker = TwitterChecker(self.logger)
+        self.w_checker = WebChecker(self.logger)
 
-loaded = False
-prev_status = GPIO.input(12)
+        self.loaded = False
+        self.prev_status = GPIO.input(12)
+
+        self.loop()
+    
+    def make_coffee(self):
+        self.loaded = False
+
+        # Turn load indicator off
+        GPIO.output(16, GPIO.LOW)
+
+        GPIO.output(18, GPIO.HIGH)
+
+        # Pause for five minutes (or how long it take to make coffee)
+        time.sleep(300)
+        GPIO.output(18, GPIO.LOW)
+
+        self.prev_status = GPIO.input(12)
+
+    def loop(self):
+        while True:
+            if self.prev_status != GPIO.input(12):
+                print(loaded)
+                loaded = True
+
+            # Set load indicator accordingly
+            GPIO.output(16, loaded)
+
+            try:
+                if loaded and (t_checker.loop() or w_checker.loop()):
+                    self.logger.debug("Coffee is being made.")
+                    self.make_coffee()
+            except KeyboardInterrupt:
+                sys.exit()
+                self.logger.warning("User interrupted.")
+            except:
+                self.logger.warning("Twitter limit hit.")
+                if w_checker.loop():
+                    self.logger.debug("Coffee is being made.")
+                    self.make_coffee()
+
+            self.prev_status = GPIO.input(12)
+            time.sleep(1)
+
 
 # Clean up
 def clean_up():
@@ -58,40 +101,4 @@ def clean_up():
 
 atexit.register(clean_up)
 
-def make_coffee():
-    loaded = False
-
-    # Turn load indicator off
-    GPIO.output(16, GPIO.LOW)
-
-    GPIO.output(18, GPIO.HIGH)
-
-    # Pause for five minutes (or how long it take to make coffee)
-    time.sleep(300)
-    GPIO.output(18, GPIO.LOW)
-
-    prev_status = GPIO.input(12)
-
-while True:
-    if prev_status != GPIO.input(12):
-        loaded = True
-
-    # Set load indicator accordingly
-    GPIO.output(16, loaded)
-
-    try:
-        if loaded and (t_checker.loop() or w_checker.loop()):
-            logger.debug("Coffee is being made.")
-            make_coffee()
-    except KeyboardInterrupt:
-        sys.exit()
-        logger.warning("User interrupted.")
-    except:
-        logger.warning("Twitter limit hit.")
-        if w_checker.loop():
-            logger.debug("Coffee is being made.")
-            make_coffee()
-
-    prev_status = GPIO.input(12)
-    time.sleep(1)
-
+machine = CoffeeMachine()
