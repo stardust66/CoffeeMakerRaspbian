94177b07a4d6f40f483620197c3b5bf9cd533696
Jason Chen <kbtpodifo@gmail.com>
2017-05-27 12:04:45 -0400

    Add reset code for loaded indicator

--- reset.py
+++ reset.py
@@ -3,9 +3,11 @@ from RPi import GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 GPIO.output(23, GPIO.LOW)
 GPIO.output(18, GPIO.LOW)
+GPIO.output(16, GPIO.LOW)
 
 # Clean up
 GPIO.cleanup()
